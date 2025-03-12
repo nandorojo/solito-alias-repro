de7f10cc373eb5b8cd94ca70a0a6b029ebc00b0a
Fernando Rojo <fernando.rojo@vercel.com>
2025-03-12 09:00:52 -0400

    first commit

---
+++ packages/app/features/home/screen.tsx
@@ -0,0 +1,90 @@
+'use client'
+
+import { TextLink } from 'solito/link'
+import { MotiLink } from 'solito/moti/app'
+import { Text, View } from 'react-native'
+import { useEffect, useId } from 'react'
+
+export function HomeScreen() {
+  const id = useId()
+  console.log('[HomeScreen] Render', id)
+  useEffect(() => {
+    console.log('[HomeScreen] useEffect')
+  }, [])
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        gap: 32,
+      }}
+    >
+      <H1>Welcome to Solito.</H1>
+      <View style={{ maxWidth: 600, gap: 16 }}>
+        <Text style={{ textAlign: 'center' }}>
+          Here is a basic starter to show you how you can navigate from one
+          screen to another. This screen uses the same code on Next.js and React
+          Native.
+        </Text>
+        <Text style={{ textAlign: 'center' }}>
+          Solito is made by{' '}
+          <TextLink
+            href="https://twitter.com/fernandotherojo"
+            target="_blank"
+            rel="noreferrer"
+            style={{ color: 'blue' }}
+          >
+            Fernando Rojo
+          </TextLink>
+          .
+        </Text>
+      </View>
+      <View style={{ flexDirection: 'row', gap: 32 }}>
+        <TextLink
+          href="/users/fernando"
+          style={{ fontSize: 16, fontWeight: 'bold', color: 'blue' }}
+        >
+          Regular Link
+        </TextLink>
+        <MotiLink
+          href="/users/fernando"
+          from={{
+            scale: 0,
+            rotateZ: '0deg',
+          }}
+          animate={({ hovered, pressed }) => {
+            'worklet'
+
+            console.log('[MotiLink] Render', { hovered, pressed })
+
+            return {
+              scale: pressed ? 0.95 : hovered ? 1.1 : 1,
+              rotateZ: pressed ? '0deg' : hovered ? '-3deg' : '0deg',
+            }
+          }}
+          transition={{
+            type: 'timing',
+            duration: 150,
+          }}
+        >
+          <Text
+            selectable={false}
+            style={{ fontSize: 16, color: 'black', fontWeight: 'bold' }}
+          >
+            Moti Link
+          </Text>
+        </MotiLink>
+      </View>
+    </View>
+  )
+}
+
+const H1 = ({ children }: { children: React.ReactNode }) => {
+  return <Text style={{ fontWeight: '800', fontSize: 24 }}>{children}</Text>
+}
+
+const P = ({ children }: { children: React.ReactNode }) => {
+  return <Text style={{ textAlign: 'center' }}>{children}</Text>
+}
---
+++ packages/app/features/user/detail-screen.tsx
@@ -0,0 +1,21 @@
+import { View, Text } from 'react-native'
+import { createParam } from 'solito'
+import { TextLink } from 'solito/link'
+
+const { useParam } = createParam<{ id: string }>()
+
+export function UserDetailScreen() {
+  const [id] = useParam('id')
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text
+        style={{ textAlign: 'center', marginBottom: 16, fontWeight: 'bold' }}
+      >
+        {`User ID: ${id}`}
+      </Text>
+
+      <TextLink href="/">👈 Go Home</TextLink>
+    </View>
+  )
+}
